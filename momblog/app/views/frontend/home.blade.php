ac1c418792e2830ea83e809142fa860ce8a2f83e
TannerHolm <holm.tanner@gmail.com>
2013-12-13 08:07:49 -0700

    first commit

---
+++ momblog/app/views/frontend/home.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Holm Family</title>
+	<link rel="stylesheet" href="/assets/css/main.css">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<header class="home-header"> Holm Family Center</header>
+<body>
+<div class="container">
+	<div class="col-md-4"><div class="circles">Tasks</div></div>
+	<div class="col-md-4"> 
+	<a href="/blog"><div class="circles">Family News</div></a></div>
+	<div class="col-md-4"><div class="circles">Events</div></div>
+</div>
+</div>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+</body>
+</html>
