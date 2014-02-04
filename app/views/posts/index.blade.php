d18ece0a0ea40a71c707929fab9ea5b5ef672b03
TannerHolm <holm.tanner@gmail.com>
2014-02-04 16:21:52 -0700

    hey

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -8,7 +8,7 @@
                 <!-- blog entry -->
                 <h1><a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                 </h1>
-                </p><i class="fa fa-calendar fa-2x"></i> {{ $post -> created_at}}</p>
+                </p><i class="fa fa-calendar fa-2x"></i> {{ $post->created_at }}</p>
                 <hr>
 @endforeach
     @stop
