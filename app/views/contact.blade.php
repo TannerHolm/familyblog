619e5f48aca069e9128660b14bd07f4ec7f173fa
Tanner Holm <holm.tanner@gmail.com>
2014-02-05 17:21:35 -0700

    text color

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
   <div class="col-lg-12">
-                <h1 style="text-align: center;">Come, and Know Us Better Man! (Or Woman, it's cool)</h1>
+                <h1 style="text-align: center; color: white;">Come, and Know Us Better Man! (Or Woman, it's cool)</h1>
                 <hr>
-                <h2 style="text-align: center;">hantanholm@gmail.com</h2>
+                <h2 style="text-align: center; color:white;">hantanholm@gmail.com</h2>
              </div> 
 
 @stop
--- app/views/layouts/single.blade.php
+++ app/views/layouts/single.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="about-us-page" lang="en">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -21,99 +21,52 @@
 
 
 <body>
-<div class="cover-all">
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                                <a class="navbar-brand" href="/posts">HanTan</a>
+    <div class="cover-all">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/posts">HanTan</a> 
 
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="/about">About</a>
-                    </li>
-                    <li><a href="/contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-<div class="blog-container">
-        <div class="row">
-                @yield('content')
-
-           
-
-     <!--        <div class="col-lg-2">
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </span>
-                    </div>
                 </div>
-            <div> -->
-                    <!-- <h4>Popular Blog Categories</h4> -->
-                    <!-- <div class="row"> -->
-<!--                         <div class="col-lg-2">
-                            <ul class="list-unstyled">
-                                <li><a href="#dinosaurs">Dinosaurs</a>
-                                </li>
-                                <li><a href="#spaceships">Spaceships</a>
-                                </li>
-                                <li><a href="#fried-foods">Fried Foods</a>
-                                </li>
-                                <li><a href="#wild-animals">Wild Animals</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-2">
-                            <ul class="list-unstyled">
-                                <li><a href="#alien-abductions">Alien Abductions</a>
-                                </li>
-                                <li><a href="#business-casual">Business Casual</a>
-                                </li>
-                                <li><a href="#robots">Robots</a>
-                                </li>
-                                <li><a href="#fireworks">Fireworks</a>
-                                </li>
-                            </ul>
-                        </div>
- -->                    <!-- </div> -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/about">About</a>
+                        </li>
+                        <li><a href="/contact">Contact</a>
+                        </li>
+                    </ul>
                 </div>
-                <!-- /well -->
-               
-                <!-- /well -->
+                <!-- /.navbar-collapse -->
             </div>
-</div>
-    
-
-
-        <footer>
+            <!-- /.container -->
+        </nav>
+        <div class="blog-container">
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Company 2014</p>
+                    @yield('content')
                 </div>
+              <!--   <div class="col-lg-4">
+                    @yield('sidebar')
+                </div> -->
             </div>
-        </footer>
+        </div>
+    </div>
 
-
-
-    <!-- /.container -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="color:white;">Copyright &copy; Company 2014</p>
+            </div>
+        </div>
+    </footer>
 
     <!-- JavaScript -->
     <script src="js/jquery-1.10.2.js"></script>
